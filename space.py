59e8c5ab6ba787f387d5e6d7fc088f187cbf193c
ekarat.st <ekaratdev@gmail.com>
2016-12-06 20:39:15 +0700

    เพิ่มspriteของgold

--- space.py
+++ space.py
@@ -15,6 +15,7 @@ class ModelSprite(arcade.Sprite):
     def sync_with_model(self):
         if self.model:
             self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
 
     def draw(self):
         self.sync_with_model()
@@ -28,14 +29,19 @@ class SpaceGameWindow(arcade.Window):
 
         self.world = World(width, height)
         self.ship_sprite = ModelSprite('images/ship.png', model=self.world.ship)
+        self.gold_sprite = ModelSprite('images/gold.png', model=self.world.gold)
 
     def on_draw(self):
         arcade.start_render()
+        self.gold_sprite.draw()
         self.ship_sprite.draw()
 
     def animate(self, delta):
         self.world.animate(delta)
         # self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
